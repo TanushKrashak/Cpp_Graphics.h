0fe5814695e68adfd55f15fc6c91559d2d935a6a
Tanush Krashak <139574093+TanushKrashak@users.noreply.github.com>
2024-04-19 14:22:47 +0530

    Update FloodFill.cpp

--- FloodFill.cpp
+++ FloodFill.cpp
@@ -8,13 +8,13 @@ using namespace std;
 //	Main Algorithm 
 //=========================================
 
-void floodfill(int x, int y, int newcolor, int oldcolor) {
+void FloodFill(int x, int y, int newcolor, int oldcolor) {
 	if (getpixel(x, y) == oldcolor) {
 		putpixel(x, y, newcolor);		
-		floodfill(x + 1, y, newcolor, oldcolor);
-		floodfill(x - 1, y, newcolor, oldcolor);
-		floodfill(x, y + 1, newcolor, oldcolor);
-		floodfill(x, y - 1, newcolor, oldcolor);
+		FloodFill(x + 1, y, newcolor, oldcolor);
+		FloodFill(x - 1, y, newcolor, oldcolor);
+		FloodFill(x, y + 1, newcolor, oldcolor);
+		FloodFill(x, y - 1, newcolor, oldcolor);
 	}	
 }
 
@@ -28,7 +28,7 @@ int main() {
 	
 	// Draw a circle and fill it with color
 	circle(200, 200, 20);
-	floodfill(200, 200, 4, 0);
+	Floodfill(200, 200, 4, 0);
 
 	getch();
 	closegraph();
