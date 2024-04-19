e8ff5f953b82a43e90db2c090907d90e06153a64
Tanush Krashak <139574093+TanushKrashak@users.noreply.github.com>
2024-04-19 14:33:06 +0530

    Create BoundaryFill.cpp

---
+++ BoundaryFill.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+#include "graphics.h"
+
+using namespace std;
+
+// =====================================
+//			Main Algorithm
+//======================================
+
+void BoundaryFill(int x, int y, int newColor, int boundaryColor) {
+	if (getpixel(x, y) != boundaryColor && getpixel(x, y) != newColor) {
+		putpixel(x, y, newColor);		
+		BoundaryFill(x + 1, y, newColor, boundaryColor);
+		BoundaryFill(x - 1, y, newColor, boundaryColor);
+		BoundaryFill(x, y + 1, newColor, boundaryColor);
+		BoundaryFill(x, y - 1, newColor, boundaryColor);
+	}	
+}
+
+// =====================================
+//		Main Algorithm End
+//======================================
+
+int main() {
+	int gd = DETECT, gm;
+	initwindow(650, 500);
+
+	// Draw a circle and fill it with color
+	circle(200, 200, 20);
+	BoundaryFill(200, 200, RED, WHITE);
+
+	getch();
+	closegraph();
+	return 0;
+}
