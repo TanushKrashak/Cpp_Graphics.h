59e484198bd51012d0baa25d316613eede53d392
Tanush Krashak <139574093+TanushKrashak@users.noreply.github.com>
2024-04-24 17:31:17 +0530

    Create FloodFill_8.cpp
    Closes #2

---
+++ FloodFill_8.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+#include "graphics.h"
+
+using namespace std;
+
+// ========================================
+//	Main Algorithm 
+//=========================================
+
+void FloodFill(int x, int y, int newColor, int oldColor) {
+	if (getpixel(x, y) == oldColor) {
+		putpixel(x, y, newColor);		
+		FloodFill(x + 1, y, newColor, oldColor);
+		FloodFill(x - 1, y, newColor, oldColor);
+		FloodFill(x, y + 1, newColor, oldColor);
+		FloodFill(x, y - 1, newColor, oldColor);
+    FloodFill(x + 1, y + 1, newColor, oldColor);
+		FloodFill(x + 1, y - 1, newColor, oldColor);
+		FloodFill(x - 1, y + 1, newColor, oldColor);
+		FloodFill(x - 1, y - 1, newColor, oldColor);
+	}	
+}
+
+// ========================================
+//	Main Algorithm End
+//=========================================
+
+int main() {
+	int gd = DETECT, gm;
+	initwindow(650, 500);
+	
+	// Draw a circle and fill it with color
+	circle(200, 200, 20);
+	Floodfill(200, 200, 4, 0);
+
+	getch();
+	closegraph();
+	return 0;
+}
